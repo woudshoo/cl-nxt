e8be4c2e351b6f61fff175f2ed42e03776a6ac36
Willem Rein Oudshoorn <woudshoo@Silver-2.local>
2009-03-24 08:52:24 +0100

    Replaced byte spec by ubyte spec

--- bluetooth.lisp
+++ bluetooth.lisp
@@ -1,20 +1,20 @@
-;
 ;; Specification:
 ;;
-;;     (byte position)   - byte = 8bits
+;;     (ubyte position)  - byte = 8bits  (== byte)
 ;;     (uword position)  - uword = 16bits, Little endian
 ;;     (string position end-position) - string is 0 terminated put in the buffer.
 ;;     (ulong position)  - ulong = 32bits, Little endian
 ;;     (data position)   - This should be the last field and the length determines how
 ;;                         long the command is, or when it is a reply, the
 ;;                         length of the reply determins the length of the data
+;;     (sbyte
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; FUNCTIONS THAT HELP DESTRUCTURING THE SPECIFICATION OF THE COMMANDS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun type-of-spec (spec)
   "Returns the type of a single variable spec for the nxt command description.
-Returns one of: 'byte, 'uword, 'string."
+Returns one of: 'ubyte, 'uword, 'string."
   (first spec))
 
 (defun last-byte-of-spec (spec)
@@ -41,7 +41,7 @@ The `args' contains key value pairs that should be taken by the function togethe
 with a type specifier.  Type specifiers are lists of the form (`type' `position' rest).
 The type is one of 
 
-  byte
+  ubyte
   uword
   string  - has additional parameter, `final-position'.
 
@@ -52,8 +52,8 @@ The result is something that looks like
 ==>
   (defun find-first (&key file-name)
       (let ((data-vector (make-array 22 :element-type 'unsigned-byte :initial-element 0)))
-          (write-byte-to-command data-vector #x01)
-          (write-byte-to-command data-vector #x86)
+          (write-ubyte-to-command data-vector #x01)
+          (write-ubyte-to-command data-vector #x86)
           (write-string-to-command data-vector file-name 2 21)
           (write-to-nxt data-vector)
           (read-nxt-reply)))
@@ -66,11 +66,11 @@ The result is something that looks like
      `(let ((data-vector (make-array ,command-length 
 				     :element-type 'unsigned-byte
 				     :initial-element 0)))
-	(write-byte-to-command data-vector ,type-code 0)
-	(write-byte-to-command data-vector ,command-code 1)
+	(write-ubyte-to-command data-vector ,type-code 0)
+	(write-ubyte-to-command data-vector ,command-code 1)
 	,(cons 'progn (loop :for (key spec) :on args :by #'cddr :collect
 	    (case (type-of-spec spec)
-	      (byte `(write-byte-to-command data-vector ,key ,(second spec)))
+	      (ubyte `(write-ubyte-to-command data-vector ,key ,(second spec)))
 	      (string `(write-string-to-command data-vector ,key ,(second spec) ,(third spec)))
 	      (uword `(write-uword-to-command data-vector ,key ,(second spec)))
 	      (t (error "Unknow type ~S in specification" (type-of-spec spec))))))
@@ -88,28 +88,28 @@ The result is something that looks like
 (def-nxt-command get-firmware-version #x01 #x88)
 (def-nxt-command get-device-info      #x01 #x9b)
 (def-nxt-command find-first           #x01 #x86 file-name (string 2 21))
-(def-nxt-command find-next            #x01 #x87 handle    (byte 2))
-(def-nxt-command nxt-close                #x01 #x84 handle     (byte 2))
+(def-nxt-command find-next            #x01 #x87 handle    (ubyte 2))
+(def-nxt-command nxt-close                #x01 #x84 handle     (ubyte 2))
 
 
-;; (def-nxt-command get-input-values     #x00 #x07 port       (byte 2))
-;; (def-nxt-command get-output-state     #x00 #x06 port       (byte 2))
+;; (def-nxt-command get-input-values     #x00 #x07 port       (ubyte 2))
+;; (def-nxt-command get-output-state     #x00 #x06 port       (ubyte 2))
 ;; (def-nxt-command get-battery-level    #x00 #x0b)
 ;; (def-nxt-command stop-sound-playback  #x00 #x0c)
-;; (def-nxt-command set-input-mode       #x00 #x05 port       (byte 2) 
-;; 		 sensor-type          (byte 3)  sensor-mode (byte 4))
+;; (def-nxt-command set-input-mode       #x00 #x05 port       (ubyte 2) 
+;; 		 sensor-type          (ubyte 3)  sensor-mode (ubyte 4))
 ;; (def-nxt-command message-read         #x00 #x13 
-;; 		 remote-inbox         (byte 2)  local-inbox (byte 3) remove (byte 4))
+;; 		 remote-inbox         (ubyte 2)  local-inbox (ubyte 3) remove (ubyte 4))
 
 ;; (def-nxt-command get-device-info      #x01 #x9b)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; HELPER FUNCTIONS TO BUILD THE COMMAND
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun write-byte-to-command (vector value location)
+(defun write-ubyte-to-command (vector value location)
   (assert (arrayp vector) (vector) "First argument should be a vector!")
   (setf (aref vector location) value))
 
-(defun read-byte-from-reply (vector location)
+(defun read-ubyte-from-reply (vector location)
   (assert (arrayp vector) (vector) "First argument should be a vector!")
   (aref vector location))
 
@@ -234,10 +234,10 @@ and send it over the wire, eh, air."
    (list (list 'code (list 'eql code))
 	 (list 'data 'vector))
    (cons 'list
-    (loop :for  (key spec) :on (append (list 'status '(byte 2))  rest) :by #'cddr :collect
+    (loop :for  (key spec) :on (append (list 'status '(ubyte 2))  rest) :by #'cddr :collect
        (list 'cons `(quote ,key)
 	     (case (type-of-spec spec)
-	       (byte `(read-byte-from-reply data ,(second spec)))
+	       (ubyte `(read-ubyte-from-reply data ,(second spec)))
 	       (string `(read-string-from-reply data ,(second spec) ,(third spec)))
 	       (uword `(read-uword-from-reply data ,(second spec)))
 	       (ulong `(read-ulong-from-reply data ,(second spec)))
@@ -245,34 +245,34 @@ and send it over the wire, eh, air."
 
 
 ;; open read command
-(def-reply-package #x80 handle (byte 3)
+(def-reply-package #x80 handle (ubyte 3)
 		        file-size (ulong 4))
 ;; oen write command
-(def-reply-package #x81 handle (byte 3))
+(def-reply-package #x81 handle (ubyte 3))
 ;; read command
 ;(def-reply-package #x82 handle (byte 3)
 ;		        amount-read (uword 4)
 ;			data (data 6))
 ;; write command
-(def-reply-package #x83 handle (byte 3)
+(def-reply-package #x83 handle (ubyte 3)
 		   amount-written-to-flash (uword 4))
 ;; close command
-(def-reply-package #x84 handle (byte 3))
+(def-reply-package #x84 handle (ubyte 3))
 ;; delete command
 (def-reply-package #x85 file-name (string 3 22))
 ;; find-first
-(def-reply-package #x86 handle (byte 3)
+(def-reply-package #x86 handle (ubyte 3)
 		        file-name (string 4 23)
 			file-size (ulong 24))
 ;; find-next
-(def-reply-package #x87 handle (byte 3)
+(def-reply-package #x87 handle (ubyte 3)
 		        file-name (string 4 23)
 			file-size (ulong 24))
 ;; Get firmware version
-(def-reply-package #x88 protocol-version-minor (byte 3)
-		        protocol-version-major (byte 4)
-                        firmware-version-minor (byte 5)
-                        firmware-version-major (byte 6))
+(def-reply-package #x88 protocol-version-minor (ubyte 3)
+		        protocol-version-major (ubyte 4)
+                        firmware-version-minor (ubyte 5)
+                        firmware-version-major (ubyte 6))
 
 ;; Get device info
 (def-reply-package #x9b 
@@ -286,19 +286,19 @@ and send it over the wire, eh, air."
 ;; MODULES
 ;;;
 (def-nxt-command request-first-module    #x01 #x90 resource-name (string 2 21))
-(def-reply-package #x90 handle (byte 3) module-name (string 4 23)
+(def-reply-package #x90 handle (ubyte 3) module-name (string 4 23)
 		   module-id (ulong 24)
 		   module-size (ulong 28)
 		   io-map-size (uword 32))
 
-(def-nxt-command request-next-module #x01 #x91 handle (byte 2))
-(def-reply-package #x91 handle (byte 3) module-name (string 4 23)
+(def-nxt-command request-next-module #x01 #x91 handle (ubyte 2))
+(def-reply-package #x91 handle (ubyte 3) module-name (string 4 23)
 		   module-id (ulong 24)
 		   module-size (ulong 28)
 		   io-map-size (uword 32))
 
-(def-nxt-command close-module-handle #x01 #x92 handle (byte 2))
-(def-reply-package #x92 handle (byte 3))
+(def-nxt-command close-module-handle #x01 #x92 handle (ubyte 2))
+(def-reply-package #x92 handle (ubyte 3))
 
 ;;;;
 ;; DIRECT COMMANDS
@@ -309,60 +309,96 @@ and send it over the wire, eh, air."
 (def-nxt-command stop-program  #00 #01)
 (def-reply-package #x01)
 
-(def-nxt-command play-sound-file #x00 #x02 loop (byte 2) file-name (string 3 22))
+(def-nxt-command play-sound-file #x00 #x02 loop (ubyte 2) file-name (string 3 22))
 (def-reply-package #x02)
 
 (def-nxt-command play-tone #x00 #x03 frequency (uword 2) duration (uword 4))
 (def-reply-package #x03)
 
 (def-nxt-command set-output-state #x00 #x04 
-		 output-port (byte 2) 
-		 power-set-point (byte 3)
-		 mode (byte 4)
-		 regulation-mode (byte 5)
-		 turn-ratio (byte 6) ;; needs to be sbyte, so this will fail with negative numebrs.
-		 run-state (byte 7)
+		 output-port (ubyte 2) 
+		 power-set-point (ubyte 3)
+		 mode (ubyte 4)
+		 regulation-mode (ubyte 5)
+		 turn-ratio (ubyte 6) ;; needs to be subyte, so this will fail with negative numebrs.
+		 run-state (ubyte 7)
 		 tacho-limit (ulong 8))
 (def-reply-package #x04)		 
 
 (def-nxt-command set-input-mode #x00 #x05
-		 input-port (byte 2)
-		 sensor-type (byte 3)
-		 sensor-mode (byte 4))
+		 input-port (ubyte 2)
+		 sensor-type (ubyte 3)
+		 sensor-mode (ubyte 4))
 (def-reply-package #x05)		 
 
 (def-nxt-command get-output-state #x00 #x06
-		 output-port (byte 2))
+		 output-port (ubyte 2))
 (def-reply-package #x06
-    output-port (byte 3)
-    power-set-point (byte 4)
-    mode (byte 5)
-    regulation-mode (byte 6)
-    turn-ratio (byte 7) ;; needs to be sbyte
-    run-state (byte 8)
+    output-port (ubyte 3)
+    power-set-point (ubyte 4)
+    mode (ubyte 5)
+    regulation-mode (ubyte 6)
+    turn-ratio (ubyte 7) ;; needs to be sbyte
+    run-state (ubyte 8)
     tacho-limit (ulong 9)
     tacho-count (ulong 13) ;; should be slong
     block-tacho-count (ulong 17) ;; should be slong)
     rotation-count (ulong 21) ;; should be slong
 )
 
-(def-nxt-command get-input-values #x00 #x07 input-port (byte 2))
+(def-nxt-command get-input-values #x00 #x07 input-port (ubyte 2))
 (def-reply-package #x07
-    input-port (byte 3)
-    valid (byte 4)
-    calibrated (byte 5)
-    sensor-type (byte 6)
-    sensor-mode (byte 7)
+    input-port (ubyte 3)
+    valid (ubyte 4)
+    calibrated (ubyte 5)
+    sensor-type (ubyte 6)
+    sensor-mode (ubyte 7)
     raw-a/d-value (uword 8)
     normalized-a/d-value (uword 10)
     scaled-value (uword 12)  ;; should be sword
     calibrated-value (uword 14) ;; should be sword
 )
 
-(def-nxt-command reset-input-scaled-value #x00 #x08 input-port (byte 2))
+(def-nxt-command reset-input-scaled-value #x00 #x08 input-port (ubyte 2))
 (def-reply-package #x08)
 
+;(def-nxt-command message-write #x00 #x09 inbox (ubyte 2) message-size (ubyte 3) message-data (data 4)) ;; Note need to think about data
+;(def-reply-package #x09)
 
+(def-nxt-command reset-motor-position #x00 #x0a output-port (ubyte 2) relative (ubyte 3))
+(def-reply-package #x0a)
+
+(def-nxt-command get-battery-level #x00 #x0b)
+(def-reply-package #x0b voltage (uword 3))
+
+(def-nxt-command stop-sound-playback #x00 #x0c)
+(def-reply-package #x0c)
+
+(def-nxt-command keep-alive #x00 #x0d)
+(def-reply-package #x0d sleep-time-limit (ulong 3))
+
+
+(def-nxt-command ls-get-status #x00 #x0e port (ubyte 2))
+(def-reply-package #x0e bytes-ready (ubyte 3))
+
+;; (def-nxt-command ls-write #x00 #x0f port (ubyte 2)
+;; 		 tx-data-length (ubyte 3)
+;; 		 rx-data-length (ubyte 4)
+;; 		 tx-data (data 5))
+;(def-reply-package #x0f)
+
+(def-nxt-command ls-read #x00 #x10 port (ubyte 2))
+;(def-reply-package #x10 bytes-read (byte 3) rx-data (data 3 19))
+		 
+
+(def-nxt-command get-current-program-name #x00 #x11)
+(def-reply-package #x11 file-name (string 3 22))
+
+(def-nxt-command message-read #x00 #x13
+		 remote-inbox (ubyte 2)
+		 local-inbox (ubyte 3)
+		 remove (ubyte 4))
+;; (def-reply-package #x13 local-inbox (byte 3) message-size (byte 4) message-data (data 5 63)) 
 
     
 
